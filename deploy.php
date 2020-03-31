c6b7bec138eb27a1c29b081963ac274c05a5eb27
Stephen Pewter <stephen.pewter@embo.org>
2020-03-31 11:40:11 +0200

    added deployer file

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'SDash');
+
+// Project repository
+set('repository', 'git@github.com:source-data/sdash.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('207.154.224.106')
+    ->set('deploy_path', '/var/www/sdash.sourcedata.io');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
