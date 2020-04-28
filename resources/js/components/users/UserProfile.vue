7cc36bb6f1b39fdd4475e808a24607332b490e8f
Orlin Topalov <orlin.topalov@sib.swiss>
2020-04-28 21:46:38 +0200

    Hide user property labels unless values are available

--- resources/js/components/users/UserProfile.vue
+++ resources/js/components/users/UserProfile.vue
@@ -3,29 +3,34 @@
         <info-bar v-if="user">
             <template v-slot:title>
                 <h1 class="pb-0 mb-0">{{ user.firstname + ' ' + user.surname }}</h1>
-                <p>{{ user.institution_name + ' &bull; ' + user.department_name }}</p>
+                <p v-if="user.institution_name">{{ user.institution_name }}</p>
             </template>
             <template v-slot:text>
-                <dl class="row">
-                    <dt class="col-sm-2">Email</dt>
-                    <dd class="col-sm-10">
-                        <a :href="'mailto:' + user.email" target="_blank" class="pb-2">{{ user.email }}</a>
-                    </dd>
-
-                    <dt class="col-sm-2">LinkedIn</dt>
-                    <dd class="col-sm-10">
-                        <a :href="user.linkedin" target="_blank" class="pb-2">{{ user.linkedin }}</a>
-                    </dd>
-
-                    <dt class="col-sm-2">Twitter</dt>
-                    <dd class="col-sm-10">
-                        <a :href="user.twitter" target="_blank" class="pb-2">{{ user.twitter }}</a>
-                    </dd>
-
-                    <dt class="col-sm-2">ORCID</dt>
-                    <dd class="col-sm-10">
-                        <a :href="'https://orcid.org/' + user.orcid" target="_blank" class="pb-2">{{ user.orcid }}</a>
-                    </dd>
+                <dl class="row mt-3">
+                    <template v-if="user.email">
+                        <dt class="col-sm-2">Email</dt>
+                        <dd class="col-sm-10">
+                            <a :href="'mailto:' + user.email" target="_blank" class="pb-2">{{ user.email }}</a>
+                        </dd>
+                    </template>
+                    <template v-if="user.linkedin">
+                        <dt class="col-sm-2">LinkedIn</dt>
+                        <dd class="col-sm-10">
+                            <a :href="user.linkedin" target="_blank" class="pb-2">{{ user.linkedin }}</a>
+                        </dd>
+                    </template>
+                    <template v-if="user.twitter">
+                        <dt class="col-sm-2">Twitter</dt>
+                        <dd class="col-sm-10">
+                            <a :href="user.twitter" target="_blank" class="pb-2">{{ user.twitter }}</a>
+                        </dd>
+                    </template>
+                    <template v-if="user.orcid">
+                        <dt class="col-sm-2">ORCID</dt>
+                        <dd class="col-sm-10">
+                            <a :href="'https://orcid.org/' + user.orcid" target="_blank" class="pb-2">{{ user.orcid }}</a>
+                        </dd>
+                    </template>
                 </dl>
             </template>
         </info-bar>
