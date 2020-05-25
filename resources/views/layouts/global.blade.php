0dc6841232a48080a6c83abedb849bf0932669fa
Orlin Topalov <orlin.topalov@sib.swiss>
2020-05-25 10:45:12 +0200

    Update link to homepage in the top navigation bar

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -22,12 +22,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand"  @auth href="{{ url('/dashboard') }}" @else  href="{{ url('/') }}" @endauth>
                 @if (Auth::check())
-                    <i class="fas fa-home"></i>
+                    <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                        <i class="fa fa-home"></i> My Dashboard
+                    </a>
                 @endif
-                    {{ config('app.name', 'Laravel') }}
-                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
