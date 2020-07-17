d025a8fc981a446d95f4d9c62e69f12420cec02c
Stephen Pewter <stephen.pewter@embo.org>
2020-07-17 15:00:30 +0200

    add images to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,7 @@
                 <blockquote>
                     <p><strong>SDash is an online open source platform for sharing of scientific results among collaborators.</strong></p>
                 </blockquote>
-                <hr/>
+                <hr />
                 <p>
                     SDash enables scientists to generate and share <em>SmartFigures</em> that link a scientific figure to the underlying source data
                     and structured machine-readable metadata. Users can manage their SmartFigures to share them with groups of colleagues or
@@ -30,6 +30,9 @@
             <div>
                 <h3>How does it work?</h3>
                 <p>
+                    <img src="/images/sdash_infographic2.jpg" alt="A flow diagram showing how SDash works." class="sdash-about--infographic">
+                </p>
+                <p>
                     Scientists have their own personal workspace where they upload their result figures by a simple drag &amp; drop.
                     Research results and underlying data are easily organized by linking them with local or remote data files,
                     computer scripts, and protocols. By creating or participating in groups of collaborating peers, researchers
@@ -37,21 +40,21 @@
                     promote critical debates and provides a way for early dissemination of findings ultimately prepare your figures for publication.
                 </p>
                 <p><em>Learn more: <a href="https://elifesciences.org/labs/9d062a9b/smartfigures-dashboard-and-editor-tools-for-quick-and-easy-research-communication">
-                    SmartFigures Dashboard and Editor: tools for quick and easy research communication</a></em></p>
+                            SmartFigures Dashboard and Editor: tools for quick and easy research communication</a></em></p>
             </div>
         </div>
 
         <aside class="col-md-4">
             <div class="p-3">
-                <p class="font-italic">SDash is developed by <a href="https://sourcedata.embo.org/">SourceData</a></p>
+                <p class="font-italic">SDash is developed by <a href="https://sourcedata.embo.org/" class="sd-sidebar-logo--link"><img src="/images/sourcedata_logo.png" alt="The SourceData logo." class="sd-sidebar-logo--image"></a></p>
             </div>
             <div class="card" style="margin-bottom: 1rem;">
                 <div class="card-body">
                     <h5 class="card-title">Do you have any questions?</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Contact</h6>
                     <p class="card-text">
-                        <strong>Hannah Sonntag</strong><br/>
-                        SDash and SourceData Coordinator<br/>
+                        <strong>Hannah Sonntag</strong><br />
+                        SDash and SourceData Coordinator<br />
                         <a href="mailto:hannah.sonntag@embo.org">hannah.sonntag@embo.org</a>
                     </p>
                 </div>
@@ -61,9 +64,13 @@
                     <h5 class="card-title" style="margin: 0;">Our partners</h5>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><a href="https://www.biologie.hu-berlin.de/en/gruppenseiten-en/sfb1315">SFB 1315 - Mechanisms and disturbances in memory consolidation</a></li>
-                    <li class="list-group-item"><a href="https://www.sib.swiss/">SIB Swiss Institute of Bioinformatics</a></li>
-                    <li class="list-group-item"><a href="#">Substance</a></li>
+                    <li class="list-group-item"><a href="https://www.biologie.hu-berlin.de/en/gruppenseiten-en/sfb1315" class="sd-sidebar--sfb-link"><img src="/images/sfb_logo.svg" alt="The SFB logo." class="sd-sidebar--sfb-logo">
+                            <div class="sd-sidebar--sfb-text">
+                                <h5>SFB 1315</h5>Mechanisms and disturbances in memory consolidation
+                            </div>
+                        </a></li>
+                    <li class="list-group-item"><a href="https://www.sib.swiss/"><img src="/images/logo_sib.png" alt="Swiss Institute of Bioinformatics" class="sd-sidebar--sib-logo"></a></li>
+                    <li class="list-group-item"><a href="https://substance.io/"><img src="/images/substance_logo.svg" alt="Substance - self-publish your research" class="sd-sidebar--substance-logo"></a></a></li>
                 </ul>
             </div>
         </aside>
--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SDash') }}</title>
     <meta name="description" content="SDash is the SourceData dashboard. Accelerate data sharing and improve research data management.">
     <!-- Scripts -->
     <script src="{{ mix('js/main.js') }}" defer></script>
@@ -18,14 +19,15 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 @if (Auth::check())
-                    <a class="navbar-brand" href="{{ url('/dashboard') }}">
-                        <i class="fa fa-home"></i> My Dashboard
-                    </a>
+                <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                    <i class="fa fa-home"></i> My Dashboard
+                </a>
                 @endif
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -43,38 +45,37 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Hi, {{ Auth::user()->firstname }} {{ Auth::user()->surname }} <span class="caret"></span>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Hi, {{ Auth::user()->firstname }} {{ Auth::user()->surname }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/dashboard">
+                                    My Dashboard
+                                </a>
+                                <a class="dropdown-item" href="{{ url('/dashboard/user/' . Auth::user()->id) }}">
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/dashboard">
-                                        My Dashboard
-                                    </a>
-                                    <a class="dropdown-item" href="{{ url('/dashboard/user/' . Auth::user()->id) }}">
-                                        Profile
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -86,4 +87,5 @@
         </main>
     </div>
 </body>
+
 </html>
