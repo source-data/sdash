66810bf86aeeea845ff865391d243ce6ea3ba4e9
Stephen Pewter <s.m.pewter@googlemail.com>
2020-08-27 13:20:55 +0200

    remove authors from list

--- resources/js/components/authors/PanelAuthorsEditForm.vue
+++ resources/js/components/authors/PanelAuthorsEditForm.vue
@@ -4,12 +4,13 @@
     <p>Edit the authors assigned to this panel.</p>
     <p>Note that adding an author assigns certain permissions to the user.</p>
     <ul class="panel-authors-edit--intro-roles">
-      <li><strong>Author</strong> - can see the panel and its details.</li>
-      <li><strong>Corresponding Author</strong> - can edit the panel and details.</li>
-      <li><strong>Curator</strong> - can see the panel and its details but is not listed in the author list.</li>
+      <li><strong>Author (Au)</strong> - can see the panel and its details.</li>
+      <li><strong>Corresponding Author (Cor)</strong> - can edit the panel and details.</li>
+      <li><strong>Curator (Cur)</strong> - can edit the panel and its details but is not listed in the author list.</li>
     </ul>
     </header>
     <section class="panel-authors-edit--list-wrapper" v-if="temporaryAuthorList">
+        <strong class="panel-authors-edit--reorder-title">Drag authors to reorder.</strong>
         <draggable
         tag="ul"
         class="panel-authors-order-list"
@@ -21,14 +22,27 @@
         >
         <transition-group type="transition" :name="!drag ? 'order-authors' : null">
          <li v-for="a in temporaryAuthorList" :key="a.order" class="panel-authors-order-list-item">
-          <strong class="panel-authors-order-list-item--name">{{a.firstname}} {{a.surname}}</strong>
+          <div class="panel-authors-order-list-item--top-bar">
+            <strong class="panel-authors-order-list-item--name">{{a.firstname}} {{a.surname}}</strong>
+            <div class="panel-authors-order-list-item--remove">
+              <b-button size="sm" variant="danger" class="panel-authors-order-list-item--remove-button"><font-awesome-icon icon="trash-alt" size="sm" @click="removeAuthor(a.id)"/></b-button>
+            </div>
+          </div>
           <em class="panel-authors-order-list-item--institution">{{a.institution_name}}</em>
-          <b-form-radio-group :options="roleOptions"></b-form-radio-group>
+          <b-form-radio-group
+           :options="roleOptions"
+            v-model="a.author_role"
+            size="small"
+            >
+          </b-form-radio-group>
            <!-- order: {{a.order}}, role: {{a.author_role}} -->
          </li>
         </transition-group>
         </draggable>
     </section>
+    <div class="panel-authors-edit--save-wrapper">
+      <b-button variant="success">Save Authors</b-button>
+    </div>
   </div>
 </template>
 
@@ -50,9 +64,9 @@ export default {
           temporaryAuthorList:[],
           drag: false,
           roleOptions: [
-            {text: "Author", value: AuthorTypes.AUTHOR},
-            {text: "Corresponding Author", value: AuthorTypes.CORRESPONDING},
-            {text: "Curator", value: AuthorTypes.CURATOR},
+            {text: "Au.", value: AuthorTypes.AUTHOR},
+            {text: "Corr.", value: AuthorTypes.CORRESPONDING},
+            {text: "Cur.", value: AuthorTypes.CURATOR},
           ],
         }
 
@@ -71,11 +85,15 @@ export default {
     },
     },
 
-    methods:{ //run as event handlers, for example
+    methods:{
+      removeAuthor(id) {
+        let index = this.temporaryAuthorList.findIndex(author => author.id === id);
+        if (index > -1) this.temporaryAuthorList.splice(index,1);
+      },
 
     },
     created() {
-      this.temporaryAuthorList = this.expandedPanelAuthors;
+      this.temporaryAuthorList = this.expandedPanelAuthors.slice();
     },
 
 }
@@ -86,13 +104,34 @@ export default {
     padding: 1rem 4rem 1rem 2rem;
   }
 
+  .panel-authors-order-list {
+    margin: 1rem 0;
+    padding: 0.4rem;
+    background-color: #666;
+}
+
   .panel-authors-order-list-item {
     display: block;
     list-style-type:none;
     margin:0 0 0.5rem 0;
-    background-color: #4a538c;
+    background-color: #343a40;
     padding: 0.5rem;
     cursor:move;
+  }
+  .panel-authors-order-list-item:last-child {
+    margin-bottom:0;
+  }
+
+  .panel-authors-order-list-item--top-bar {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    position: relative;
+  }
+  .panel-authors-order-list-item--remove-button {
+    padding: 0.25rem 0.25rem;
+    font-size: 0.875rem;
+    line-height: 0.5;
   }
 
   .order-authors-move {
@@ -105,12 +144,23 @@ export default {
 
   .ghost {
   opacity: 0.6;
-  box-shadow:0 2px 1rem -2px yellow;
+  box-shadow:0 0 1rem blue;
 }
 
 .panel-authors-order-list-item--name {
     display: block;
     line-height: 1;
-    color: #d6dfee;
 }
+
+.panel-authors-order-list-item--institution {
+  font-size:0.85em;
+}
+
+.panel-authors-order-list-item--remove {
+    position: absolute;
+    right: -4px;
+    top: -5px;
+}
+
+
 </style>
