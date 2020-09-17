a62de4380088cdd98086d0d184ba83df36b842a9
Stephen Pewter <s.m.pewter@googlemail.com>
2020-09-17 13:21:10 +0200

    Test - owner can modify external author list

--- tests/Feature/API/Author/ExternalAuthorTest.php
+++ tests/Feature/API/Author/ExternalAuthorTest.php
@@ -17,12 +17,13 @@ class ExternalAuthorTest extends TestCase
     private $user;
     private $panel;
     private $externalAuthor;
+    private $externalAuthorData;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->user = factory(User::class)->create();
-        $externalAuthorData = [
+        $this->externalAuthorData = [
             'firstname'         => 'Pasta',
             'surname'           => 'Sauce',
             'email'             => 'pasta.sauce@spaghetti.net',
@@ -30,7 +31,7 @@ class ExternalAuthorTest extends TestCase
             'department_name'   => 'School of Excellence',
             'orcid'             => '0000-0002-6470-0634',
         ];
-        $this->externalAuthor = ExternalAuthor::create($externalAuthorData);
+        $this->externalAuthor = ExternalAuthor::create($this->externalAuthorData);
 
         $this->panel = factory(Panel::class)->create(['user_id' => $this->user->id]);
         $this->panel->authors()->attach($this->user, ['order' => 0, 'role' => User::PANEL_ROLE_CORRESPONDING_AUTHOR]);
@@ -45,5 +46,35 @@ class ExternalAuthorTest extends TestCase
 
         $response->assertJson(["DATA" => [0 => ["external_authors" => [0 => ["email" => $this->externalAuthor->email]]]]]);
         $response->assertJson(["DATA" => [0 => ["external_authors" => [0 => ["orcid" => $this->externalAuthor->orcid]]]]]);
+    }
+
+    public function testPanelOwnerCanModifyExternalAuthors()
+    {
+        $currentUser = [
+            'id'            => $this->user->id,
+            'order'         => 0,
+            'origin'        => 'users',
+            'author_role'   => User::PANEL_ROLE_CORRESPONDING_AUTHOR,
+            'email'         => $this->user->email
+        ];
+
+        $extraExternalAuthor = [
+            'origin'            => 'external',
+            'author_role'       => 'author',
+            'email'             => 'xkk4slajd@dsjf.eom',
+            'firstname'         => 'Stepton',
+            'surname'           => 'Pantalonne',
+            'institution_name'  => 'Paris Sorbonne',
+            'department_name'   => 'Department of Geology',
+            'orcid'             => '1dk02-2dojs-219djasl-2',
+            'order'             => 1
+        ];
+
+        $response = $this->actingAs($this->user, 'api')->put('api/panels/' . $this->panel->id . '/authors', ['authors' => [$currentUser, $extraExternalAuthor]]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson(["DATA" => ["external_authors" => [0 => ["email" => $extraExternalAuthor["email"]]]]]);
+        $response->assertJson(["DATA" => ["external_authors" => [0 => ["orcid" => $extraExternalAuthor["orcid"]]]]]);
     }
 }
