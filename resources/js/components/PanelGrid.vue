503f0aa4c4560371afede7474b7b438bb525ec3e
Thomas Eidens <thomas.eidens@embo.org>
2021-10-28 09:53:14 +0200

    Add call-to-action footer in Dashboard view

--- resources/js/components/PanelGrid.vue
+++ resources/js/components/PanelGrid.vue
@@ -107,6 +107,47 @@
             </div>
         </b-container>
 
+        <footer id="sd-panel-grid-footer">
+            <section v-if="isLoggedIn" id="sd-entice-logged-in-user" class="container-fluid bg-primary">
+                <b-row>
+                    <b-col md="6" class="call-to-action-text">
+                        <div class="text-light text-lg font-weight-bolder">
+                            Do you want to share your results within a special group?
+                        </div>
+
+                        <div class="text-lg font-weight-bolder">
+                            <router-link :to="{name: 'creategroup'}">
+                                Start a group.
+                            </router-link>
+                        </div>
+                    </b-col>
+
+                    <b-col md="6" class="call-to-action-image d-none d-md-block">
+                        <img src="/images/group.svg" alt="">
+                    </b-col>
+                </b-row>
+            </section>
+
+            <section v-else id="sd-entice-guest" class="container-fluid bg-primary">
+                <b-row>
+                    <b-col md="5" class="call-to-action-text">
+                        <div class="text-lg font-weight-bolder">
+                            <span class="text-light">Share research results</span> with
+                            your colleagues, the science community, and the world!
+                        </div>
+
+                        <div>
+                            <router-link :to="{name: 'register'}">Register here</router-link> and get started.
+                        </div>
+                    </b-col>
+
+                    <b-col md="7" class="call-to-action-image d-none d-md-block">
+                        <img src="/images/lupe.svg" alt="">
+                    </b-col>
+                </b-row>
+            </section>
+        </footer>
+
         <lightbox
             :visible="isLightboxOpen"
             :imgs="'/panels/' + expandedPanel.id + '/image'"
@@ -145,6 +186,7 @@ export default {
         ...mapGetters([
             "isLoggedIn",
             "isLoadingPanels",
+            "isLoggedIn",
             "hasPanels",
             "loadedPanels",
             "hasLoadedAllResults",
@@ -242,6 +284,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import 'resources/sass/_colors.scss';
+
 .no-panel-alert {
     max-width: 480px;
     margin: 0 auto;
@@ -329,5 +373,30 @@ export default {
     #sd-featured-jumbotron {
         margin: 50px 10vw;
     }
+}
+
+#sd-panel-grid-footer > * {
+    padding: 5vw 7.5vw;
+}
+
+.call-to-action-text {
+    margin: auto 0;
+    padding-right: 5vw;
+}
+.call-to-action-text a {
+    color: $mostly-black-blue;
+    text-decoration: underline;
+}
+.call-to-action-image {
+    margin: auto 0;
+    padding: 0 5vw;
+}
+.call-to-action-image img {
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+}
+#sd-entice-logged-in-user .call-to-action-image img {
+    max-width: 25vw;
 }
 </style>
