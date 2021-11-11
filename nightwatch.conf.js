d0ed71151924610d7af702565999098522d7294f
Thomas Eidens <thomas.eidens@embo.org>
2021-11-11 13:29:53 +0100

    Read SDash URL from .env file

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,40 +1,39 @@
-const chromedriver = require('chromedriver')
-const geckodriver = require('geckodriver')
+const dotenv = require("dotenv")
+dotenv.config()
 
 const output_folder = "tests/js/integration/output"
-
-const chrome_webdriver_config = {
-    start_process: true,
-    port: 9515,
-    server_path: chromedriver.path,
-    log_path: output_folder,
-}
-const firefox_webdriver_config = {
-    start_process: true,
-    port: 4444,
-    server_path: geckodriver.path,
-    log_path: output_folder,
-}
-
-const firefox_test_settings = {
-    desiredCapabilities: {
-        browserName: 'firefox'
-    },
-}
-const chrome_test_settings = {
-    desiredCapabilities: {
-        browserName: 'chrome',
-    },
-    webdriver: chrome_webdriver_config,
-}
 
 module.exports = {
     src_folders: ["tests/js/integration"],
     output_folder: output_folder,
-    webdriver: firefox_webdriver_config,
+
+    // Common configuration options for the webdriver
+    webdriver: {
+        log_path: output_folder,
+        start_process: true
+    },
+
     test_settings: {
-        chrome: chrome_test_settings,
-        default: firefox_test_settings,
-        firefox: firefox_test_settings,
+        default: {
+            launch_url: process.env.APP_URL // read from the .env file
+        },
+        chrome: {
+            desiredCapabilities: {
+                browserName: "chrome",
+            },
+            webdriver: {
+                port: 9515,
+                server_path: require("chromedriver").path,
+            },
+        },
+        firefox: {
+            desiredCapabilities: {
+                browserName: "firefox"
+            },
+            webdriver: {
+                port: 4444,
+                server_path: require("geckodriver").path,
+            }
+        },
     }
 }
