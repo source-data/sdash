7f4a9c8a7842aca98971b0eb41da71ec69bffda7
Thomas Eidens <thomas.eidens@embo.org>
2021-12-09 10:51:59 +0100

    Update deploy script after change from public to private repo
    When downloading the frontend build asset during deployments we used to
    need a Github personal access token to access the private sdash repo.
    After making that repo public we can now download the asset without any
    auth.

--- .github/scripts/gh-dl-release.sh
+++ .github/scripts/gh-dl-release.sh
@@ -3,7 +3,7 @@
 # gh-dl-release! It works!
 # 
 # This script downloads an asset from latest or specific Github release of a
-# private repo. Feel free to extract more of the variables into command line
+# public repo. Feel free to extract more of the variables into command line
 # parameters.
 #
 # PREREQUISITES
@@ -13,7 +13,7 @@ usage() {
   cat << EOF
 Usage: $0 <repo identifier> <tag name> <asset name>
 
-Download an asset from a private Github repository. Requires a Github OAuth token with "repo" permissions in the file ~./secrets.
+Download an asset from a public Github repository.
 
 repo identifier:    Identifier of the Github repository from where to download the asset, in the form of <author>/<repository name>. Example: source-data/sdash
 tag name:           The version of the asset to download. Enter "latest" to fetch the most recent tag.
@@ -36,13 +36,8 @@ if [ "${VERSION}" != "latest" ]; then
     VERSION="tags/${VERSION}"
 fi
 
-TOKEN=`cat ~/.secrets/GITHUB_ACCESS_TOKEN`
-function gh_curl() {
-  curl -H "Authorization: token $TOKEN" $@
-}
-
 REPO_URL="https://api.github.com/repos/${REPO_ID}"
-ASSET_DATA=`gh_curl -H "Accept: application/vnd.github.v3+json" -s $REPO_URL/releases/$VERSION`
+ASSET_DATA=`curl -H "Accept: application/vnd.github.v3+json" -s $REPO_URL/releases/$VERSION`
 ASSET_ID=`echo ${ASSET_DATA} | jq ".assets | map(select(.name == \"${ASSET_NAME}\")) | first.id"`
 echo "ASSET_ID: ${ASSET_ID}"
 
@@ -51,4 +46,4 @@ if [ "$ASSET_ID" = "null" ]; then
   exit 1
 fi;
 
-gh_curl --location -OJ -H "Accept:application/octet-stream" $REPO_URL/releases/assets/$ASSET_ID
+curl --location -OJ -H "Accept:application/octet-stream" $REPO_URL/releases/assets/$ASSET_ID
