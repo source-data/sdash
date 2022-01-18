52ef1f3083ae9a9c487ba4c0e36cc4402443ab9e
Thomas Eidens <thomas.eidens@embo.org>
2022-01-18 15:09:59 +0100

    Fix e2e tests

--- tests/e2e/pages/login.js
+++ tests/e2e/pages/login.js
@@ -9,12 +9,6 @@ var loginCommands = {
             .setValue('@password', password)
             .assert.enabled('@submit')
             .click('@submit')
-            .waitForElementVisible(
-                '@notification',
-                2000,
-                false, /* don't fail if the notification doesn't become visible, the login might still have worked */
-            );
-        this.api.saveScreenshot(`tests/e2e/output/login/${Date.now()}_login-notification.png`);
         return this.waitForElementVisible('#sd-panel-listing-grid', 10000);
     }
 };
@@ -28,8 +22,5 @@ module.exports = {
         userName: 'input#login-email',
         password: 'input#login-password',
         submit: 'button[type="submit"]',
-        notification: '.snotifyToast',
-        successNotification: '.snotifyToast.snotify-success',
-        errorNotification: '.snotifyToast.snotify-error',
     }
 };
