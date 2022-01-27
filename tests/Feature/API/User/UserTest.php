c7bbdf1f31ad4a9ee700df6f24aec77302d1398c
Thomas Eidens <thomas.eidens@embo.org>
2022-01-27 11:39:04 +0100

    Refactored API tests related to user avatars

--- tests/Feature/API/User/UserTest.php
+++ tests/Feature/API/User/UserTest.php
@@ -19,7 +19,7 @@ class UserTest extends TestCase
         return $this->actingAs($user, 'sanctum')->getJson('/api/users/me');
     }
 
-    private function deleteAvatarAsUser(User $target, User $actor)
+    private function deleteAvatarAsUser(User $target, $actor)
     {
         $base = $this;
         if ($actor) {
@@ -68,17 +68,30 @@ class UserTest extends TestCase
         $response->assertUnauthorized();
     }
 
+    private function assertAvatarPresent(User $user, String $message)
+    {
+        $userDetails = $this->getUserProfile($user);
+        $this->assertEquals($userDetails['DATA']['avatar'], $user->avatar, $message);
+    }
+  
+    private function assertNoAvatar(User $user, String $message)
+    {
+        $userDetails = $this->getUserProfile($user);
+        $this->assertEmpty($userDetails['DATA']['avatar'], $message);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
     public function an_authenticated_user_can_delete_their_avatar()
     {
-        $userWithAvatar = $this->userWithAvatar;
-        $responseProfileBeforeDelete = $this->getUserProfile($userWithAvatar);
-        $this->assertEquals($responseProfileBeforeDelete['DATA']['avatar'], $userWithAvatar->avatar);
+        $user = $this->userWithAvatar;
 
-        $responseDeleteAvatar = $this->deleteAvatar($userWithAvatar);
-        $responseDeleteAvatar->assertStatus(200);
-
-        $responseProfileAfterDelete = $this->getUserProfile($userWithAvatar);
-        $this->assertEmpty($responseProfileAfterDelete['DATA']['avatar'], 'expected user to not have an avatar after deletion');
+        $this->assertAvatarPresent($user, 'expected user to have an avatar before deletion');
+        $this->deleteAvatar($user)->assertStatus(200);
+        $this->assertNoAvatar($user, 'expected user to not have an avatar after deletion');
     }
 
     /**
@@ -88,15 +101,11 @@ class UserTest extends TestCase
      */
     public function an_authenticated_user_can_delete_their_non_existent_avatar()
     {
-        $userWithoutAvatar = $this->user;
-        $responseProfileBeforeDelete = $this->getUserProfile($userWithoutAvatar);
-        $this->assertEmpty($responseProfileBeforeDelete['DATA']['avatar'], 'expected user to not have an avatar before deletion');
+        $user = $this->user;
 
-        $responseDeleteAvatar = $this->deleteAvatar($userWithoutAvatar);
-        $responseDeleteAvatar->assertStatus(200);
-
-        $responseProfileAfterDelete = $this->getUserProfile($userWithoutAvatar);
-        $this->assertEmpty($responseProfileAfterDelete['DATA']['avatar'], 'expected user to still not have an avatar after deletion');
+        $this->assertNoAvatar($user, 'expected user to not have an avatar before deletion');
+        $this->deleteAvatar($user)->assertStatus(200);
+        $this->assertNoAvatar($user, 'expected user to still not have an avatar after deletion');
     }
     /**
      * @test
@@ -105,8 +114,14 @@ class UserTest extends TestCase
      */
     public function an_unauthenticated_user_cannot_delete_an_avatar()
     {
-        $responseDeleteAvatar = $this->deleteJson('/api/users/' . $this->userWithAvatar->id . '/avatar');
-        $responseDeleteAvatar->assertUnauthorized();
+        $user = $this->userWithAvatar;
+        $actor = null;
+
+        // no check that the avatar is present before deletion because there is apparently no good way to reset
+        // authentication once it is set by the actingAs function. The only way to remain not-logged-in is to not log
+        // in at all.
+        $this->deleteAvatarAsUser($user, $actor)->assertUnauthorized();
+        $this->assertAvatarPresent($user, 'expected user to still have an avatar after unauthorized deletion');
     }
     /**
      * @test
@@ -115,7 +130,11 @@ class UserTest extends TestCase
      */
     public function an_authenticated_user_cannot_delete_another_users_avatar()
     {
-        $responseDeleteAvatar = $this->deleteAvatarAsUser($this->userWithAvatar, $this->user);
-        $responseDeleteAvatar->assertForbidden();
+        $user = $this->userWithAvatar;
+        $actor = $this->user;
+
+        $this->assertAvatarPresent($user, 'expected user to have an avatar before deletion');
+        $this->deleteAvatarAsUser($user, $actor)->assertForbidden();
+        $this->assertAvatarPresent($user, 'expected user to still have an avatar after forbidden deletion');
     }
 }
