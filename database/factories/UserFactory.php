a02748a9cfcec66d832c90a7dcc51503263c6172
Stephen Pewter <s.m.pewter@googlemail.com>
2022-01-27 13:36:03 +0100

    update user factory to include user_slug

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,5 +29,6 @@ $factory->define(User::class, function (Faker $faker) {
         'department_name' => $faker->sentence(5),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'user_slug' => Str::uuid(),
     ];
 });
