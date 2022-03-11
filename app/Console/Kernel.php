ace2f2b3e0f7b405a8540bfa6c36e12c4f2ee7df
Thomas Eidens <thomas.eidens@embo.org>
2022-03-11 11:16:44 +0100

    Added console command to get simple statistics

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\RestoreBackup::class,
+        \App\Console\Commands\SDashStats::class,
     ];
 
     /**
