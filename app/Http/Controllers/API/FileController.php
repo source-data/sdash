source-data/sdash

commit 4ff247f38b58ea86bc201f460107bac8c3212aa4

Show better error messages for failed file uploads

app/Http/Controllers/API/FileController.php
@@ -46,10 +46,15 @@ class FileController extends Controller
     {
         $user = auth()->user();
 
-        $request->validate([
-            'file'  => ['required_without:url', 'max:4096'],
-            'url'   => ['required_without:file', 'url']
-        ]);
+        $maxFileSizeInMB = 4;
+        $maxFileSizeInBytes = 4 * 1000;
+        $rules = [
+            'file' => ['required', 'mimes:jpeg,png,jpg,gif,pdf,tif', "max:$maxFileSizeInBytes"],
+        ];
+        $messages = [
+            'file.max' => "Source files may not be larger than $maxFileSizeInMB MB",
+        ];
+        $this->validate($request, $rules, $messages);
 
         if (Gate::allows('modify-panel', $panel)) {
             

app/Http/Controllers/API/PanelController.php
@@ -145,9 +145,16 @@ class PanelController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'file' => ['required', 'mimes:jpeg,png,jpg,gif,pdf,tif', 'max:4096']
-        ]);
+        $maxFileSizeInMegaBytes = 4;
+        $maxFileSizeInKiloBytes = 4 * 1000;
+        $rules = [
+            'file' => ['required_without:url', "max:$maxFileSizeInKiloBytes"],
+            'url'  => ['required_without:file', 'url'],
+        ];
+        $messages = [
+            'file.max' => "SmartFigure images may not be larger than $maxFileSizeInMegaBytes MB",
+        ];
+        $this->validate($request, $rules, $messages);
 
         $user = auth()->user();
 

resources/js/components/files/FileUploads.vue
@@ -352,7 +352,7 @@ export default {
                 this.$store.commit('setPendingUpload', false);
                 this.file = null
             }).catch(error => {
-                this.$snotify.error(error.data.message, "Upload failed")
+                this.$snotify.error(error.data.errors.file[0], "Upload failed")
                 this.$store.commit('setPendingUpload', false);
             })
         },

commit 86fe0f684c6612ce174f2205f8ec9fd4d5cb727a

Fixed validation of SmartFigure source URLs
4ff247f introduced a regression in the validation for adding a source URL
to a SmartFigure: where either a URL or file upload used to be required,
now a file upload had to be present if a URL was to be set.
This commit reverts the behavior back to requiring a URL or a file.

app/Http/Controllers/API/FileController.php
@@ -49,7 +49,8 @@ class FileController extends Controller
         $maxFileSizeInMB = 4;
         $maxFileSizeInBytes = 4 * 1000;
         $rules = [
-            'file' => ['required', 'mimes:jpeg,png,jpg,gif,pdf,tif', "max:$maxFileSizeInBytes"],
+            'file' => ['required_without:url', 'mimes:jpeg,png,jpg,gif,pdf,tif', "max:$maxFileSizeInBytes"],
+            'url'   => ['required_without:file', 'url']
         ];
         $messages = [
             'file.max' => "Source files may not be larger than $maxFileSizeInMB MB",